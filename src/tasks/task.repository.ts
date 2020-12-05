56c20cac2e6a30b0daeb8dd14abad67578ec2878
Gin58 <invincible.gnj1875@gmail.com>
2020-12-06 01:57:06 +0900

    get task for user

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -7,9 +7,14 @@ import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(
+    filterDto: GetTasksFilterDto,
+    user: User
+  ): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder(`task`);
+
+    query.where('task.userId = :userId', { userId: user.id });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
@@ -22,6 +27,7 @@ export class TaskRepository extends Repository<Task> {
     const tasks = await query.getMany();
     return tasks;
   }
+
   async createTask(
     createTaskDto: CreateTaskDto,
     user: User
